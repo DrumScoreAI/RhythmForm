4e8d3db0efead9a16d838c5a14cd90b773324d16
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-11-25 19:39:03 +0000

    use appimage --extract, fuse blocked

--- scripts/mscore_batch_convert.sh
+++ scripts/mscore_batch_convert.sh
@@ -4,18 +4,21 @@ if [ -z "$RHYTHMFORMHOME" ]; then
   echo "Error: RHYTHMFORMHOME environment variable is not set."
   exit 1
 fi
-if [ ! -d "$RHYTHMFORMHOME/training_data/musicxml" ]; then
-  echo "Error: Directory $RHYTHMFORMHOME/training_data/musicxml does not exist."
+
+$TRAINING_DATA_DIR="$RHYTHMFORMHOME/training_data"
+
+if [ ! -d "$TRAINING_DATA_DIR/musicxml" ]; then
+  echo "Error: Directory $TRAINING_DATA_DIR/musicxml does not exist."
   exit 1
 fi
 
 # Create the pdfs directory if it doesn't exist
-mkdir -p $RHYTHMFORMHOME/training_data/pdfs
+mkdir -p $TRAINING_DATA_DIR/pdfs
 
 # Loop through all xml files and convert them to PDF in the parallel pdfs directory
 echo "Converting XML files to PDF..."
-for file in $RHYTHMFORMHOME/training_data/musicxml/*.xml; do
+for file in $TRAINING_DATA_DIR/musicxml/*.xml; do
   filename="${file##*/}"
-  $MUSESCORE_PATH -o "$RHYTHMFORMHOME/training_data/pdfs/${filename%.xml}.pdf" -r 300 "$file"
+  $MUSESCORE_PATH -o "$TRAINING_DATA_DIR/pdfs/${filename%.xml}.pdf" -r 300 "$file"
 done
 echo "Conversion complete."

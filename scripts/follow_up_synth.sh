28801172c0a69ca67dd3bc642753159118323133
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-12-06 12:43:39 +0000

    add follow-up script

---
+++ scripts/follow_up_synth.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Default values
+
+TRAINING_DATA_DIR="$RHYTHMFORMHOME/training_data"
+
+num_cores=$1
+
+echo "Manifest file created at $TRAINING_DATA_DIR/training_data.csv."
+
+# Prepare data for training
+echo "Preparing data for training using $num_cores cores..."
+python prepare_dataset.py --cores "$num_cores"
+echo "Data preparation complete."
+
+# Run tokenizer
+echo "Running tokenizer (serial)..."
+python -m omr_model.tokenizer
+echo "Tokenizer run complete."
+
+# CHMOD training data
+echo "Setting permissions for training data using $num_cores cores..."
+find $TRAINING_DATA_DIR -type f -print0 | xargs -P "$num_cores" -0 -I {} chmod 666 {}
+find $TRAINING_DATA_DIR -type d -print0 | xargs -P "$num_cores" -0 -I {} chmod 777 {}
+echo "Permissions set."
+
+echo "All data synthesis tasks completed successfully."
+echo "You are ready to train the OMR model."

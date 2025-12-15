44bc3fcc914ec13a2ad7f13dee9083f845bc074c
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-12-15 17:52:08 +0000

    sort sorting

--- scripts/synthesizer.sh
+++ scripts/synthesizer.sh
@@ -131,7 +131,7 @@ fi
 temp1=$RANDOM.tmp
 temp2=$RANDOM.tmp
 temp3=$RANDOM.tmp
-find $TRAINING_DATA_DIR/musicxml -name "*.xml" | grep -v altered > $temp1
+find $TRAINING_DATA_DIR/musicxml -name "*.xml" | grep -v altered | sort -V > $temp1
 
 # Generate synthetic scores
 echo "Generating synthetic scores using $num_cores cores..."
@@ -144,7 +144,7 @@ fi
 echo "Synthetic score generation complete."
 
 if [ "$continuation" == "true" ] && [ "$existing_scores" -gt 0 ]; then
-    find $TRAINING_DATA_DIR/musicxml -name "*.xml" | grep -v altered | sort > $temp2
+    find $TRAINING_DATA_DIR/musicxml -name "*.xml" | grep -v altered | sort -V > $temp2
     comm -13 $temp1 $temp2 > $temp3
     # rm -f $temp1 $temp2
 else

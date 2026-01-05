9a19079888c6055990a2c023dbac9f4a92770048
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-01-05 15:45:47 +0000

    fix logging date format

--- scripts/omr_model/train.py
+++ scripts/omr_model/train.py
@@ -31,6 +31,7 @@ def setup_logging(log_file, log_to_stdout=False):
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
         handlers=handlers
     )
 

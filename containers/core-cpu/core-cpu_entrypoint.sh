3972d92035c82702698f58bf9639272173b3fd0a
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-01-30 12:28:01 +0000

    add prmon

--- containers/core-cpu/core-cpu_entrypoint.sh
+++ containers/core-cpu/core-cpu_entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source /app/venv/bin/activate
-exec "$@"
+
+/app/prmon/bin/prmon --filename /app/training_data/logs/prmon.txt \
+    --json-summary /app/training_data/logs/prmon.json --interval 10  \
+    --log-filename prmon.log] [--interval 30] \
+    --fast-memmon \
+    -- exec "$@"

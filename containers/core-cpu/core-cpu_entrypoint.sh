52f0e7960488ec35360062526efdcdcce097cd2c
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-01-30 12:48:04 +0000

    typo in prmon args

--- containers/core-cpu/core-cpu_entrypoint.sh
+++ containers/core-cpu/core-cpu_entrypoint.sh
@@ -3,6 +3,6 @@ source /app/venv/bin/activate
 
 /app/prmon/bin/prmon --filename /app/training_data/logs/prmon.txt \
     --json-summary /app/training_data/logs/prmon.json --interval 10  \
-    --log-filename prmon.log] [--interval 30] \
+    --log-filename prmon.log --interval 30 \
     --fast-memmon \
     -- exec "$@"

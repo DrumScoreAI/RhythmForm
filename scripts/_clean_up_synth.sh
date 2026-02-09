04be5426d476a02e46d89b56b73b6913f06af52b
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-02-09 11:10:20 +0000

    account for multiple pages

--- scripts/_clean_up_synth.sh
+++ scripts/_clean_up_synth.sh
@@ -11,25 +11,26 @@ dryrun=$3
 bname=$(basename $xml)
 
 name=${bname%.xml}
-image=`ls images/$name*.png 2>/dev/null`
+image=`ls images/$name*.png 2>/dev/null | awk '{print $1}'`
+images=`ls images/$name*.png 2>/dev/null`
 pdf=`ls pdfs/$name.pdf 2>/dev/null`
-smt=`ls smt/$name*.smt 2>/dev/null`
+smts=`ls smt/$name*.smt 2>/dev/null`
 if [ -z $pdf ] || [ -z $image ]
 then
         if [ "$dryrun" != "dryrun" ]; then
             rm $xml 2>/dev/null
-            rm $image 2>/dev/null
+            rm $images 2>/dev/null
             rm $pdf 2>/dev/null
-            rm $smt 2>/dev/null
+            rm $smts 2>/dev/null
             rm musicxml/$name.json 2>/dev/null
             rm musicxml/${name}_altered.xml 2>/dev/null
         else
             echo $bname has no pdf or image
             echo "Dry run mode - not deleting files."
             echo "Would remove: $xml"
-            echo "Would remove: $image"
+            echo "Would remove: $images"
             echo "Would remove: $pdf"
-            echo "Would remove: $smt"
+            echo "Would remove: $smts"
             echo "Would remove: musicxml/$name.json if present"
             echo "Would remove: musicxml/${name}_altered.xml if present"
         fi
